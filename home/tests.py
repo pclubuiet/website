1e49f45c10881b337f4db2923732a37a44893d79
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2017-10-03 18:00:41 +0530

    home/tests.py: Add tests
    Tests to check successful rendering of homepage and
    resource page are added. Travis CI will execute these
    tests before building.

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class WebsiteTests(TestCase):
+
+    def test_homepage(self):	#checks if homepage would be rendered properly
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_resourcepage(self):	#checks if resources page would be rendered properly
+        response = self.client.get('/home/resources/')
+        self.assertEqual(response.status_code, 200)
