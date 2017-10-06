ce643e1afa7c07424f020cb052211013f0c5728a
Harshit Jindal <harshit.jindal@icloud.com>
2017-10-06 07:39:24 +0530

    Make "/" redirect to "/home"
    Fixes Issue https://github.com/pclubuiet/website/issues/66

--- pclubWebsite/urls.py
+++ pclubWebsite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/', include('home.urls')),
+    url(r'^$', RedirectView.as_view(url='/home/')),
 ]
