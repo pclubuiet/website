db6a0fd045d830df98da36612a02f07fa886fb21
Abdullah <abdullahkhan0503@gmail.com>
2017-10-08 19:26:06 +0000

    Implement a custom error 404 page.
     Added a 404 page template along with its CSS styling; added the server-side class-based error 404 handling.

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, handler404
 from . import views
+
 app_name='home'
 urlpatterns = [
 url(r'^$', views.Home.as_view(), name = 'home'),
 url(r'^resources/', views.Resources.as_view(), name = 'resources')
 ]
+
+handler404 = 'Template404.as_view()'
+
