ba3624bd0d004fb285fbb1db8eafa8f4d214e440
jamestthompson3 <jamestthompson3@gmail.com>
2017-10-20 00:21:25 +0300

    added resources model, form, view

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django import forms
 from django.core import validators
+from .models import *
 
 class UserForm(forms.ModelForm):
 	password=forms.CharField(widget=forms.PasswordInput())
@@ -16,3 +17,8 @@ class UserForm(forms.ModelForm):
 		else:
 			pass
 		return username
+
+class ProfileForm(forms.ModelForm):
+	class Meta():
+		model = UserProfileInfo
+		fields = ('full_name','year','branch','interests','phone_number')
--- home/views.py
+++ home/views.py
@@ -8,27 +8,53 @@ class Template404(TemplateView):
 class Home(TemplateView):
     template_name = 'home/home.html'
 
-class Resources(TemplateView):
-    template_name = 'home/resources/resources.html'
+class Resources(View):
+	validated = False
+	
+	def get(self, request):
+		profile_form = ProfileForm
+		
+		return render(request,'home/resources/resources.html',
+			{'profile_form':profile_form,'validated':self.validated})
+
+	def post(self, request):
+		profile_form = ProfileForm(data=request.POST)
+		if profile_form.is_valid():
+			profile=profile_form.save(commit=False)
+			profile.user=request.user
+			profile.save()
+			self.validated=True
+		
+		else:
+			print(profile_form.errors)
+
+		return render(request,'home/resources/resources.html',
+			{'profile_form':profile_form, 'validated':self.validated})
 
 class Register(View):
 	registered = False
 	
 	def get(self, request):
 		user_form=UserForm()
+		profile_form = ProfileForm
 		
 		return render(request,'home/register/register.html',
-			{'user_form':user_form,'registered':self.registered})
+			{'user_form':user_form,'profile_form':profile_form,'registered':self.registered})
 
 	def post(self, request):
 		user_form = UserForm(data=request.POST)
-		if user_form.is_valid():
+		profile_form = ProfileForm(data=request.POST)
+		if user_form.is_valid() and profile_form.is_valid():
 			user=user_form.save()
 			user.set_password(user.password)
 			user.save()
+			profile=profile_form.save(commit=False)
+			profile.user=user
+			profile.save()
 			self.registered=True
+		
 		else:
 			print(user_form.errors, profile_form.errors)
 
 		return render(request,'home/register/register.html',
-			{'user_form':user_form, 'registered':self.registered})
+			{'user_form':user_form,'profile_form':profile_form, 'registered':self.registered})
