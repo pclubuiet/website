ab4ead9bf56080b7c4236df4ba492451752bf5f4
gurpreetsingh00885 <gurpreetsingh00885@gmail.com>
2018-03-21 14:04:23 +0530

    Resources: Add model and cards (#140)
    * Add Resource model and register it on admin
    
    * Add resources as cards to templates

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Resource(models.Model):
+    title = models.CharField(max_length=128, blank=False, null=True)
+    url = models.URLField(max_length=128, db_index=True, blank=False, null=True)
+
+    def __str__(self):
+    	return self.title
--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
+from django import views
+from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import *
+
 
 class Template404(TemplateView):
     template_name = "404.html"
@@ -6,5 +10,6 @@ class Template404(TemplateView):
 class Home(TemplateView):
     template_name = 'home/home.html'
 
-class Resources(TemplateView):
-    template_name = 'home/resources/resources.html'
+class Resources(views.View):
+    def get(self, request, *args, **kwargs):
+    	return render(request, "home/resources/resources.html", {'resources': Resource.objects.all()})
