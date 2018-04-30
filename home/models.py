68af846c65b2d7882cecaceea752ec1a4199a417
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2018-04-30 18:05:57 +0530

    Add ResourceURL model and view
    Clicking on a resource card redirects to
    a new page which further has a list which
    redirects to different resources for a
    particular 'topic'

--- home/models.py
+++ home/models.py
@@ -3,7 +3,17 @@ from django.db import models
 
 class Resource(models.Model):
     title = models.CharField(max_length=128, blank=False, null=True)
-    url = models.URLField(max_length=128, db_index=True, blank=False, null=True)
-
+    
     def __str__(self):
     	return self.title
+
+class ResourceURL(models.Model):
+	title = models.CharField(max_length=128, blank=False, null=True)
+	url = models.URLField(max_length=128, db_index=True, blank=False, null=True)
+	resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+	category = models.CharField(max_length=128, blank=False, null=True, choices=(("video", "Videos"),
+																				 ("blog", "Blogs / Articles"),
+																				 ("ebook", "E-Books"),
+																				 ("other", "Others"),))
+	def __str__(self):
+		return self.title + " (" + str(self.resource) + ")"
