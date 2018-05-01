f43a1156ea7a9501fb208f451a34bc5a819b1bc2
Sagar Kalra <sagar.kalra98@hotmail.com>
2018-05-01 15:15:27 +0530

    Implement type change depending on date

--- home/models.py
+++ home/models.py
@@ -13,6 +13,8 @@ class Events(models.Model):
     image=models.FileField(upload_to='events_images/')
     brief = models.CharField(max_length=500, blank=False, null=True)
     url = models.URLField(max_length=128, db_index=True, blank=False, null=True)
+    date=models.DateField()
+    typeofentry=models.CharField(max_length=20, default='Suggestion')
 
     def __str__(self):
         return self.title
--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django import views
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import *
+from datetime import date
 
 
 class Template404(TemplateView):
@@ -16,4 +17,8 @@ class Resources(views.View):
 
 class events(views.View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'home/events/events.html', {'events': Events.objects.all()})
+        events = Events.objects.all()
+        for event in events:
+            if event.date < date.today():
+                event.typeofentry='Past Events'
+        return render(request, 'home/events/events.html', {'events': events})
