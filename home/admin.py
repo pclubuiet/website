0c066bfc9b6bcac62c6b2b388dbe0e5c2d4df17e
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2018-05-07 00:11:35 +0530

    Add permission to Event model
    Users provided with the permission can view
    all suggested events on events page and approve
    them. Other users can see their unapproved
    suggestions instead. Also change typeofentry field
    to a choice field.

--- home/admin.py
+++ home/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Resource)
-admin.site.register(Events)
+admin.site.register(Event)
--- home/models.py
+++ home/models.py
@@ -8,13 +8,19 @@ class Resource(models.Model):
     def __str__(self):
     	return self.title
 
-class Events(models.Model):
+class Event(models.Model):
+    class Meta:
+        permissions = (
+            ('approve_suggestion', 'Approve a suggested event'),
+        )
     title = models.CharField(max_length=128, blank=False, null=True)
     image=models.FileField(upload_to='events_images/')
     brief = models.CharField(max_length=500, blank=False, null=True)
     url = models.URLField(max_length=128, db_index=True, blank=False, null=True)
     date=models.DateField()
-    typeofentry=models.CharField(max_length=20, default='Suggestion')
+    typeofentry=models.CharField(max_length=20, choices = (("past", "Past Event"),
+                                                           ("upcoming", "Upcoming Event"),
+                                                           ("suggested", "Suggested Event")) , default='suggested')
     suggested_by = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
