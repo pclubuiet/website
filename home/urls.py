80388381e497a6332392b97eba1ca7c02cfa3883
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2018-08-20 10:25:47 +0530

    Add blog list template

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,8 @@ app_name='home'
 urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('resources/<int:pk>/', views.Resources.as_view(), name = 'resources'),
+    path('blog/<int:pk>/', views.BlogPostView.as_view(), name = 'blog_post'),
+    path('blog/', views.BlogPostList.as_view(), name = 'blog'),
     path('topics/', views.Topics.as_view(), name = 'topics'),
     path('', views.Home.as_view(), name = 'home'),
 ]
